e87e946b30e93aa3185cb7b27867d61fc5f1644c
Bodong <604772159@qq.com>
2026-08-15 12:58:00 +0800

    feat: add controller_state.h (copied from nRF52840)

--- wireless/bs21/src/main.c
+++ wireless/bs21/src/main.c
@@ -1,7 +1,10 @@
 #include <stdio.h>
+#include "controller_state.h"
 
 int main(void)
 {
+    struct controller_state state = {0};
     printf("BS21 Receiver starting...\n");
+    printf("controller_state size: %zu bytes\n", sizeof(state));
     return 0;
 }
