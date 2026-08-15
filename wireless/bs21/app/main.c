4f36e6c80ad7b9bbf57a5faedf1700de29998d84
Bodong <604772159@qq.com>
2026-08-15 21:40:43 +0800

    feat: add app and sdk-compat components (replace SDK demo)

---
+++ wireless/bs21/app/main.c
@@ -0,0 +1,24 @@
+#include "soc_osal.h"
+#include "app_init.h"
+
+static void *hello_task(const char *arg)
+{
+    (void)arg;
+    while (1) {
+        osal_printk("flydigi app running\r\n");
+        osal_msleep(1000);
+    }
+    return NULL;
+}
+
+static void app_entry(void)
+{
+    osal_kthread_lock();
+    osal_task *t = osal_kthread_create((osal_kthread_handler)hello_task, 0, "hello", 0x1000);
+    if (t != NULL) {
+        osal_kthread_set_priority(t, 24);
+    }
+    osal_kthread_unlock();
+}
+
+app_run(app_entry);
