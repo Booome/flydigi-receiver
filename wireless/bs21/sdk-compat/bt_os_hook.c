4f75e52471228dca356f6463e41aa63c1961c34e
Bodong <604772159@qq.com>
2026-08-16 14:34:28 +0800

    debug: hook osal_vmalloc to log 80-100 byte allocs

--- wireless/bs21/sdk-compat/bt_os_hook.c
+++ wireless/bs21/sdk-compat/bt_os_hook.c
@@ -2,6 +2,7 @@
 
 void *__real_bt_os_new(unsigned int size);
 int __real_uapi_gle_set_adv(void *ctx);
+void *__real_osal_vmalloc(unsigned long size);
 
 static void *g_bt_os_49;
 
@@ -25,4 +26,13 @@ int __wrap_uapi_gle_set_adv(void *ctx)
     osal_printk("uapi_gle_set_adv ctx: b[4]=%u b[37]=%u b[38]=%u b[39]=%u b[40]=%u len=%u\r\n",
                 b[4], b[37], b[38], b[39], b[40], b[37] | (b[38] << 8));
     return __real_uapi_gle_set_adv(ctx);
+}
+
+void *__wrap_osal_vmalloc(unsigned long size)
+{
+    void *p = __real_osal_vmalloc(size);
+    if (size >= 80 && size <= 100) {
+        osal_printk("osal_vmalloc(%lu)=%p\r\n", size, p);
+    }
+    return p;
 }
