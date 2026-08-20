dcc4e6b5b028cf3a4d99c25a9d9fb5b047b55d12
Bodong <604772159@qq.com>
2026-08-20 08:07:40 +0800

    feat(default): add NV connection record storage module (conn_nv)

---
+++ wireless/bs21/apps/default/conn_nv.h
@@ -0,0 +1,12 @@
+#ifndef CONN_NV_H
+#define CONN_NV_H
+
+#include <stdbool.h>
+#include <stdint.h>
+
+void conn_nv_init(void);
+bool conn_nv_load(uint8_t addr_out[6]);
+bool conn_nv_save(const uint8_t addr[6]);
+bool conn_nv_is_fatal(void);
+
+#endif /* CONN_NV_H */
